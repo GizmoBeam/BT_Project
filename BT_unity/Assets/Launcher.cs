fd0601d6720709ecd04ee080552450da7afbcb60
GogleEarth <32813163+GogleEarth@users.noreply.github.com>
2020-01-11 17:53:53 +0900

    200111
    플레이버튼 누르면 서버에 연결하게 만듬

---
+++ BT_unity/Assets/Launcher.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace Com.MyCompany.MyGame
+{
+    public class Launcher : MonoBehaviourPunCallbacks
+    {
+        #region Private Serializable Fields
+        string gameVersion = "1";
+        [SerializeField]
+        private byte maxPlayerPerRoom = 8;
+        #endregion
+
+        void Awake()
+        {
+            PhotonNetwork.AutomaticallySyncScene = true;
+        }
+        void Start()
+        {
+            
+        }
+        void Update()
+        {
+            
+        }
+        public void Connect()
+        {
+            if (PhotonNetwork.IsConnected)
+            {
+                PhotonNetwork.JoinRandomRoom();
+            }
+            else
+            { 
+                PhotonNetwork.GameVersion = gameVersion;
+                PhotonNetwork.ConnectUsingSettings();
+            }
+        }
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
+            PhotonNetwork.CreateRoom(null, new RoomOptions());
+        }
+        public override void OnDisconnected(DisconnectCause cause)
+        {
+            Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+        }
+
+        public override void OnJoinRandomFailed(short returnCode, string message)
+        {
+            Debug.Log("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+            PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayerPerRoom });
+        }
+
+        public override void OnJoinedRoom()
+        {
+            Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
+        }
+    }
+}
