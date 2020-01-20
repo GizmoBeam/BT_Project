a9ecdfd183db8bee742d86623984f3d44d2f8053
GogleEarth <32813163+GogleEarth@users.noreply.github.com>
2020-01-20 13:40:43 +0900

    200120
    이거저거 수정

--- BT_unity/Assets/PlayerManager.cs
+++ BT_unity/Assets/PlayerManager.cs
@@ -14,12 +14,12 @@ namespace Com.MyCompany.MyGame
         {
             if(stream.IsWriting)
             {
-                stream.SendNext(Health);
+                stream.SendNext(curr_Hp);
                 stream.SendNext(currSpeed);
             }
             else
             {
-                this.Health = (float)stream.ReceiveNext();
+                this.curr_Hp = (float)stream.ReceiveNext();
                 this.currSpeed = (float)stream.ReceiveNext();
             }
         }
@@ -27,30 +27,97 @@ namespace Com.MyCompany.MyGame
         #endregion
 
         #region Public Fields
-
-        public float Health = 1f;
         public static GameObject LocalPlayerInstance;
 
+        public float curr_Hp;
+        public float curr_Mp;
+
         [SerializeField]
-        public float maxSpeed = 20f;
+        public float maxSpeed = 5f;
         [SerializeField]
-        public float aclrt = 5f;
+        public float aclrt = 3f;
 
         #endregion
 
-        #region Private Fields
+        #region SerializeField
 
         [SerializeField]
-        private GameObject beams;
+        int Lv = 1;
+        [SerializeField]
+        float max_Hp = 500f;
+        [SerializeField]
+        float max_Mp = 250f;
+        [SerializeField]
+        int max_Exp = 100;
+        [SerializeField]
+        float Atk = 10f;
+        [SerializeField]
+        float Def = 2f;
+        [SerializeField]
+        float Str = 5f;
+        [SerializeField]
+        float Dex = 5f;
+        [SerializeField]
+        float Int = 5f;
+        [SerializeField]
+        int Main_Stat;
+        [SerializeField]
+        float Str_per_LV = 1.5f;
+        [SerializeField]
+        float Dex_per_LV = 1.5f;
+        [SerializeField]
+        float Int_per_LV = 1.5f;
+        [SerializeField]
+        float Attack_Speed = 1.5f;
+        #endregion
+
+        #region Private Method
+        int curr_Exp = 0;
+
+        float additional_Atk;
+        float additional_Def;
+        float additional_Attack_Speed;
+        float additional_Move_Speed;
+        float additional_Str;
+        float additional_Dex;
+        float additional_Int;
 
         private CharacterController characterController;
         private Animator animator;
 
-        bool IsMoving;
         Vector3 targetPos;
         Vector3 dir;
-        float distance;
         float currSpeed;
+
+        void Init()
+        {
+            max_Hp += Str * 25f;
+            max_Mp += Int * 20f;
+
+            curr_Hp = max_Hp;
+            curr_Mp = max_Mp;
+
+            float mainstat;
+            if (Main_Stat == 0)
+                mainstat = Str;
+            else if (Main_Stat == 1)
+                mainstat = Dex;
+            else
+                mainstat = Int;
+
+            Atk += mainstat * 2;
+            Def += Dex * 0.01f;
+        }
+
+        void LevelUp()
+        {
+            Lv++;
+            Str += Str_per_LV;
+            Dex += Dex_per_LV;
+            Int += Int_per_LV;
+            curr_Exp -= max_Exp;
+            max_Exp += Lv * 100;
+        }
 
         #endregion
 
@@ -73,14 +140,6 @@ namespace Com.MyCompany.MyGame
 
         void Awake()
         {
-            if (beams == null)
-            {
-                Debug.LogError("<Color=Red><a>Missing</a></Color> Beams Reference.", this);
-            }
-            else
-            {
-                beams.SetActive(false);
-            }
             if(photonView.IsMine)
             {
                 PlayerManager.LocalPlayerInstance = this.gameObject;
@@ -93,6 +152,8 @@ namespace Com.MyCompany.MyGame
             CameraWork _cameraWork = this.gameObject.GetComponent<CameraWork>();
             characterController = GetComponent<CharacterController>();
             animator = GetComponent<Animator>();
+
+            Init();
 
             if (_cameraWork != null)
             {
@@ -116,19 +177,22 @@ namespace Com.MyCompany.MyGame
 
         void Update()
         {
-            if (photonView.IsMine)
-            {
+            //if (photonView.IsMine)
+            //{
                 ProcessInputs();
 
                 transform.rotation = Quaternion.LookRotation(dir);
                 characterController.Move(dir * currSpeed * Time.deltaTime);
                 animator.SetFloat("Speed", currSpeed);
-            }
+            //}
 
-            if (Health <= 0f)
+            if (curr_Hp <= 0f)
             {
                 GameManager.Instance.LeaveRoom();
             }
+
+            if (curr_Exp >= max_Exp)
+                LevelUp();
         }
 
         void OnTriggerEnter(Collider other)
@@ -137,12 +201,8 @@ namespace Com.MyCompany.MyGame
             {
                 return;
             }
-            
-            if (!other.name.Contains("Beam"))
-            {
-                return;
-            }
-            Health -= 0.1f;
+
+            curr_Hp -= 0.1f;
         }
 
         void OnTriggerStay(Collider other)
@@ -152,12 +212,7 @@ namespace Com.MyCompany.MyGame
                 return;
             }
             
-            if (!other.name.Contains("Beam"))
-            {
-                return;
-            }
-            
-            Health -= 0.1f * Time.deltaTime;
+            curr_Hp -= 0.1f * Time.deltaTime;
         }
 
         #endregion
@@ -181,7 +236,6 @@ namespace Com.MyCompany.MyGame
                     dir.y = 0f;
                     currSpeed = Mathf.Clamp(currSpeed += aclrt * Time.deltaTime, 0f, maxSpeed);
                 }
-                IsMoving = true;
             }
             else
             {
