e68017b0d3be5920a04e7a9722fd561b3401011a
GogleEarth <32813163+GogleEarth@users.noreply.github.com>
2020-01-23 17:24:06 +0900

    200123
    게임 화면에서 채팅추가

---
+++ BT_unity/Assets/ChatManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+public class ChatManager : MonoBehaviour
+{
+    #region PublicFields
+
+    public Text[] ChatText;
+    public InputField ChatInput;
+
+    #endregion
+
+    #region 채팅
+
+    public PhotonView PV;
+
+    public void Send()
+    {
+        string msg = PhotonNetwork.NickName + " : " + ChatInput.text;
+        PV.RPC("ChatRPC", RpcTarget.All, msg);
+        ChatInput.text = "";
+    }
+
+    [PunRPC] // RPC는 플레이어가 속해있는 방 모든 인원에게 전달한다
+    void ChatRPC(string msg)
+    {
+        bool isInput = false;
+        for (int i = 0; i < ChatText.Length; i++)
+            if (ChatText[i].text == "")
+            {
+                isInput = true;
+                ChatText[i].text = msg;
+                break;
+            }
+        if (!isInput) // 꽉차면 한칸씩 위로 올림
+        {
+            for (int i = 1; i < ChatText.Length; i++) ChatText[i - 1].text = ChatText[i].text;
+            ChatText[ChatText.Length - 1].text = msg;
+        }
+    }
+    #endregion
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChatInput.text = "";
+        for (int i = 0; i < ChatText.Length; i++) ChatText[i].text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            if (ChatInput.text == "")
+            {
+                ChatInput.Select();
+            }
+            else
+            {
+                Send();
+            }
+        }
+    }
+}
